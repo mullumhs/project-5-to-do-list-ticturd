49a3055aa5b204b6e683e4550210bcc8f5542eef
ticturd <148925085+ticturd@users.noreply.github.com>
2025-03-19 10:13:10 +1100

    finished lab2

--- lab2.py
+++ lab2.py
@@ -12,18 +12,20 @@ fruits = ["Grape", "Apple", "Lemon", "Cherry", "Date", "Elderberry", "Fig",  "Ho
 
 
 # Remove the third fruit from the list using the pop() method.
-
+fruits.pop(2)
 
 # Use the remove() method to remove a fruit of your choice from the list.
-
+fruits.remove("Banana")
 
 # Search for "Apple" in the list and print a message saying whether or not it was found.
-
+if "Apple" in fruits:
+    print("Apple found")
 
 # Sort the List in alphabetical order.
-
+fruits.sort()
 
 # Create a new list that contains a slice of the first 5 items
-
+newfruits = fruits[0:5]
 
 # Print out the new sliced list using a for loop
+print(newfruits)
