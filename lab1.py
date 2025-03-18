336f8fcc342f2d457c4ddd7b6ba604744ad29df3
ticturd <148925085+ticturd@users.noreply.github.com>
2025-03-19 10:04:14 +1100

    finished lab1

--- lab1.py
+++ lab1.py
@@ -8,18 +8,23 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a list named favorite_foods and add at least five different food items to it
-
+favorite_foods = ["emit", "leon", "ward", "jenga", "bill"]
 
 # Change the third item in the list to a different food
-
+favorite_foods[2] = "sinclair"
 
 # Print out only the first and fourth items in the list
+print(favorite_foods[0])
+print(favorite_foods[3])
 
 
 # Ask the user for a food item
-
+food = (input("Please enter a food to add to the list."))
 
 # Append the user's food item to the list
-
+favorite_foods.append(food)
 
 # Print out the entire list of food items
+for bitchunt in favorite_foods:
+    print(bitchunt)
+    
