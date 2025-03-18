403276fe274161c986206c07d425a467bc421670
ticturd <148925085+ticturd@users.noreply.github.com>
2025-03-19 10:40:16 +1100

    started to do list

--- to-do-list.py
+++ to-do-list.py
@@ -6,3 +6,32 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#make a list
+#allow user to add variables to the list
+#allow user to delete variables from the list
+#allow user to edit the list
+
+def menu():
+    print("Please select an option:")
+    print("1. Add something to the list")
+    print("2. Delete something from the list")
+    print("3. Edit something from the list")
+
+    while True:
+        try:
+            choice = input()
+            int(choice)
+            
+
+        except:
+            print("Please enter an integer")
+
+
+
+
+
+
+
+
+
+
